fc9474980bba36fd23ae1345c8e1a80796e40196
Jeni405 <103121020+Jeni405@users.noreply.github.com>
2024-09-19 09:39:17 -0400

    Delete Desktop/Portfolio/sample/portfolio directory

--- Desktop/Portfolio/sample/portfolio/src/Fontawesome.jsx
+++
@@ -1,7 +0,0 @@
-// src/fontAwesome.js
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
-library.add(fas);
-library.add(faGithub);
